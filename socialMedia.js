963ba1c818f743fb063899edc7028902a81321e6
Joan Pujols <joanpujols1998@gmail.com>
2019-05-15 22:13:23 -0400

    Updating

--- socialMedia.js
+++ socialMedia.js
@@ -21,4 +21,25 @@ class Message{
 user1 = new User ('michael@fish.com', 'yeeer')
 user2 = new User ('roy@fish.com', 'yeeer2')
 
-// to create a new message
+let user1form = document.getElementById('user1-form')
+let user2inbox = document.getElementById('user2-inbox')
+window.addEventListener('keydown', function(event){
+  if(event.key == 'Shift' && event.which == 13)
+  if(event.which == 13){
+    sendMessage()
+  }
+})
+
+function sendMessage(){
+  let text = document.getElementById('msg-content')
+  let p = document.createElement('p')
+  p.innerText = text.value
+  user2inbox.appendChild(p)
+  text.value = ''
+  text.focus()
+}
+
+user1form.addEventListener('submit', function(event){
+  event.preventDefault()
+  sendMessage()
+})
